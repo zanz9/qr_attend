2b8f7bd3df8092e77484ddaeefa0f9beb4e41a65
zanz9 <baidosovich@gmail.com>
2024-04-14 22:55:01 +0500

    server build file observer

--- server/main.js
+++ server/main.js
@@ -7,6 +7,7 @@ const port = 3000
 require('dotenv').config()
 
 const exceptionMiddleware = require('./router/exception')
+const path = require("path");
 
 app.use(express.json())
 app.use(cookieParser())
@@ -14,6 +15,8 @@ app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
 }))
+
+app.use('/', express.static(path.resolve(__dirname, 'dist')))
 
 app.use('/api', require('./router/router'))
 
